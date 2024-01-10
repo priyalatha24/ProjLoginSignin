d6354bcaaef96f4770cd7357c2c249f4f791e476
Priyalatha <priyalatha24042002@gmail.com>
2024-01-10 21:13:31 +0530

    popuploginsigin

---
+++ newproj/navbar.js
@@ -0,0 +1,28 @@
+let openLoginPopup = document.getElementById('popup')
+
+// let openLoginPopup = document.getElementById('popup2');
+
+function openLoginPopup(){
+    popup.classList.add('btn')
+}
+
+function openSigninPopup(){
+    popup.classList.remove('btn2')
+}
+
+
+// Get references to the popups
+// let loginPopup = document.getElementById('popup');
+// let signinPopup = document.getElementById('popup2');
+
+// // Function to open login popup
+// function openLoginPopup() {
+//     loginPopup.classList.remove('hidden');
+//     signinPopup.classList.add('hidden');
+// }
+
+// // Function to open signin popup
+// function openSigninPopup() {
+//     signinPopup.classList.remove('hidden');
+//     loginPopup.classList.add('hidden');
+// }
